87bd47fa472c7df3c8c82829267375614db8d376
Ikhwan S.H <ikhwansyatricha@gmail.com>
2024-10-04 21:26:36 +0700

    Fix indentation.

--- main.go
+++ main.go
@@ -17,19 +17,19 @@ var ALLOWED_ORIGINS = []string{"http://localhost:3000"}
 func main() {
 	fmt.Println("Starting back-end server.")
 
-  router := mux.NewRouter()
+    router := mux.NewRouter()
 
 	router.HandleFunc("/", handler.Welcome_handler)
 	router.HandleFunc("/login", handler.Login_handler)
 	router.HandleFunc("/session_check", handler.CheckSession_handler)
 	router.HandleFunc("/refresh_session", handler.RefreshSession_handler)
-	router.HandleFunc("/logout", handler.Logout_handler)
+    router.HandleFunc("/logout", handler.Logout_handler)
 
-  // Cors Options
-  headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Content-Language", "Origin"})
-  originsOk := handlers.AllowedOrigins(ALLOWED_ORIGINS)
-  methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
-  credentialsOK := handlers.AllowCredentials()
+    // Cors Options
+    headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Content-Language", "Origin"})
+    originsOk := handlers.AllowedOrigins(ALLOWED_ORIGINS)
+    methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+    credentialsOK := handlers.AllowCredentials()
 
 	hostname := "localhost"
 	if len(HOST) > 0 {
@@ -37,9 +37,9 @@ func main() {
 	}
 	log.Printf("listening to http://%s:%s", hostname, PORT)
 	log.Fatal(
-    http.ListenAndServe(
-      fmt.Sprintf("%s:%s", HOST, PORT),
-      handlers.CORS(headersOk, originsOk, methodsOk, credentialsOK)(router),
-    ),
-  )
+        http.ListenAndServe(
+            fmt.Sprintf("%s:%s", HOST, PORT),
+            handlers.CORS(headersOk, originsOk, methodsOk, credentialsOK)(router),
+        ),
+    )
 }
